5316faf7f72e9e69cf2e1621bcc45458da9cbbba
Oliver Schwarz <oliver.schwarz@gmail.com>
2016-05-20 10:56:56 +0200

    More grunt stuff and ignore settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,22 @@
-// Gruntfile.js
-
-'use strict';
-
 module.exports = function(grunt) {
-
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'src/css/style.css' : 'src/scss/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
   });
 
-};
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('default',['watch']);
+}
