6280927799cdead5156372786c277c275fe9dcd1
Oliver Schwarz <oliver.schwarz@gmail.com>
2016-05-20 12:58:53 +0200

    Adding htmllint

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,33 @@ module.exports = function(grunt) {
       }
     },
     watch: {
+      html: {
+        files: ['src/index.html'],
+        tasks: ['htmlhint']
+      },
       css: {
         files: '**/*.scss',
         tasks: ['sass']
+      }
+    },
+    htmlhint: {
+      build: {
+        options: {
+          'tag-pair': true,
+          'tagname-lowercase': true,
+          'attr-lowercase': true,
+          'attr-value-double-quotes': true,
+          'doctype-first': true,
+          'spec-char-escape': true,
+          'id-unique': true,
+          'head-script-disabled': true,
+          'style-disabled': true
+        },
+        src: ['src/index.html']
       }
     }
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
   grunt.registerTask('default',['watch']);
 }
